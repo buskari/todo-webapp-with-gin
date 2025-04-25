d2d8ccd5aa536ccb43b94538b8d1eb8aae23ae2c
Andris Buscariolli <andrisbuscariolli88@gmail.com>
2025-04-25 16:50:17 -0300

    refactor: BuildDB method and project structure

---
+++ internal/infra/persistence/inmemorydb.go
@@ -0,0 +1,43 @@
+package persistence
+
+import (
+	"encoding/csv"
+	"fmt"
+	"log"
+	"os"
+	"sync"
+
+	"github.com/buskari/todo-webapp-with-gin/internal/domain"
+)
+
+var (
+	todoDB []*domain.Todo
+	once   sync.Once
+)
+
+func BuildDB() []*domain.Todo {
+	once.Do(func() {
+		file, err := os.Open("data/todos_seed.csv")
+		if err != nil {
+			log.Fatalf("Could not read from source file: %v", err)
+		}
+		defer file.Close()
+
+		reader := csv.NewReader(file)
+
+		tasks, err := reader.ReadAll()
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		todoDB = []*domain.Todo{}
+		for _, task := range tasks {
+			todo := domain.NewTodo(task[0])
+			todoDB = append(todoDB, todo)
+		}
+
+		fmt.Printf("Loaded %d ToDos from CSV\n", len(todoDB))
+	})
+
+	return todoDB
+}
