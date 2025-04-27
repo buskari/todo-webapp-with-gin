f8c536f40d77211a72c2bb3912f4fd6e6c1e2272
Andris Buscariolli <andrisbuscariolli88@gmail.com>
2025-04-27 14:16:10 -0300

    refactor: keep things simple to follow the go way and avoid uncessary code

--- internal/domain/todo.go
+++ internal/domain/todo.go
@@ -4,42 +4,34 @@ var lastID int = 0
 
 type Todo struct {
 	id        int
-	title     string
+	Title     string
 	completed bool
 }
 
-// constructor
 func NewTodo(title string) *Todo {
 	return &Todo{
 		id:        generateID(),
-		title:     title,
+		Title:     title,
 		completed: false,
 	}
 }
 
-// getters
 func (t *Todo) ID() int {
 	return t.id
 }
 
-func (t *Todo) Title() string {
-	return t.title
-}
-
-func (t *Todo) IsCompleted() bool {
-	return t.completed
-}
-
-// Setters
-func (t *Todo) SetTitle(title string) {
-	t.title = title
+func (t *Todo) MarkAsCompleted() {
+	t.completed = true
 }
 
 func (t *Todo) ToggleCompleted() {
 	t.completed = !t.completed
 }
 
-// internal
+func (t *Todo) IsCompleted() bool {
+	return t.completed
+}
+
 func generateID() int {
 	lastID++
 	return lastID
